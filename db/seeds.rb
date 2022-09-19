95793c7e1b4ef224e60e3c6025c8e66c53b8a841
cooldj101 <dsavory22@gmail.com>
2022-09-19 15:20:42 +0000

    installed the seed_dump (by the gemfile)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.create!([
+  {title: "harry Potter", author: "someone", release: "2022-09-19 00:00:00", price: "12.03"},
+  {title: "next book", author: "other person", release: "2022-09-19 00:00:00", price: "43.0"}
+])
+User.create!([
+  {username: "some person"},
+  {username: "another person"}
+])
+UserBook.create!([
+  {user_id: 2, book_id: 2}
+])
