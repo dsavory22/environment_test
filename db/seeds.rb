e3bdc129e91a68e9f7a860ab61821d626ac656bb
cooldj101 <dsavory22@gmail.com>
2022-09-19 15:24:42 +0000

    just filled with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 Book.create!([
   {title: "harry Potter", author: "someone", release: "2022-09-19 00:00:00", price: "12.03"},
-  {title: "next book", author: "other person", release: "2022-09-19 00:00:00", price: "43.0"}
+  {title: "next book", author: "other person", release: "2022-09-19 00:00:00", price: "43.0"},
+  {title: "next next book", author: "other other person", release: "2022-09-19 00:00:00", price: "44.0"}
 ])
 User.create!([
   {username: "some person"},
